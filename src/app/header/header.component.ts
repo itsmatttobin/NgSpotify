7897dcd4a27da075b4cc5b089cd008384ec828c1
Matt Tobin <matt@matttobin.co.uk>
2018-08-04 21:07:07 +0100

    Added some basic styles

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,11 +7,30 @@ import { UserService } from '../services/user.service';
   templateUrl: './header.component.html'
 })
 export class HeaderComponent implements OnInit {
+  private links: Array<NavLink>;
+  private userId: string;
 
   constructor(
     private router: Router,
     private userService: UserService
-  ) { }
+  ) {
+    this.links = [
+      {
+        name: 'Dashboard',
+        link: '/'
+      },
+      {
+        name: 'Artists',
+        link: '/artists'
+      },
+      {
+        name: 'Tracks',
+        link: '/tracks'
+      }
+    ];
+
+    this.userId = this.userService.getUserProfile().id;
+  }
 
   ngOnInit() {
   }
@@ -21,4 +40,9 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['/']);
   }
 
+}
+
+export interface NavLink {
+  name: string;
+  link: string;
 }
