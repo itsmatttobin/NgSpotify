c3a4f5bc3a8dc360a520fe7bde662155d3c3b4cf
Matt Tobin <matt@matttobin.co.uk>
2018-08-04 23:48:16 +0100

    Updated environments and login links

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  private loginUrl: string;
 
   constructor(
     private userService: UserService
-  ) { }
+  ) {
+    this.loginUrl = environment.loginUrl;
+  }
 
   ngOnInit() {
   }
