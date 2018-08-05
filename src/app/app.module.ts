b55eb5f751061c6bace4b118cfc29414e25c267a
Matt Tobin <matt@matttobin.co.uk>
2018-08-05 16:45:00 +0100

    Added collapsable playlist card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -24,7 +25,8 @@ import { PlaylistsComponent } from './playlists/playlists.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/playlists/playlists.component.ts
+++ src/app/playlists/playlists.component.ts
@@ -1,21 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { PlaylistsService } from '../services/playlists.service';
+import { faChevronRight, faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-playlists',
   templateUrl: './playlists.component.html'
 })
 export class PlaylistsComponent implements OnInit {
+  private chevron: any;
+  private playlistCardActive: boolean;
 
   constructor(
     private playlistsService: PlaylistsService
-  ) { }
+  ) {
+    this.chevron = faChevronRight;
+    this.playlistCardActive = true;
+  }
 
   ngOnInit() {
   }
 
   private createPlaylist(): void {
     this.playlistsService.createPlaylist('this is the name', false);
+  }
+
+  private togglePlaylistCard(): void {
+    this.playlistCardActive = !this.playlistCardActive;
+
+    if (this.playlistCardActive) {
+      this.chevron = faChevronRight;
+    } else {
+      this.chevron = faChevronLeft;
+    }
   }
 
 }
