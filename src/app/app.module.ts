63fe9a393ff99558c2efa355cf082079112b8ff3
Matt Tobin <matt@matttobin.co.uk>
2018-08-07 19:17:26 +0100

    Added links to footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { TracksComponent } from './tracks/tracks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { HeaderComponent } from './header/header.component';
 import { PlaylistsComponent } from './playlists/playlists.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { PlaylistsComponent } from './playlists/playlists.component';
     TracksComponent,
     NotFoundComponent,
     HeaderComponent,
-    PlaylistsComponent
+    PlaylistsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
